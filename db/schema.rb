4e02ffa6122ffa735e5f3429c96030cf0cda9679
Ann-Marie Khor <ann.mariekhor@gmail.com>
2016-08-04 18:19:32 +0800

    consultant edit works

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20160804060038) do
   enable_extension "plpgsql"
 
   create_table "consultants", force: :cascade do |t|
-    t.integer  "users_id"
+    t.integer  "user_id"
     t.string   "qualifications"
     t.string   "languages"
     t.float    "ratings",        default: 0.0
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20160804060038) do
     t.integer  "class_size",     default: 1
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
-    t.index ["users_id"], name: "index_consultants_on_users_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,5 +44,4 @@ ActiveRecord::Schema.define(version: 20160804060038) do
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
 
-  add_foreign_key "consultants", "users", column: "users_id"
 end
