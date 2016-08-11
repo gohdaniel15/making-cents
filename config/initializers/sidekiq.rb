34bb4b7ba9833ad233d178330eecbffa1af572eb
gohdaniel15 <gohdaniel15@gmail.com>
2016-08-11 12:59:17 +0800

    changed sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,17 @@
-Sidekiq.configure_client do |config|
-  config.redis = { :url => 'redis://127.0.0.1:6379/1' }
-end
+unless Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { :url => 'redis://127.0.0.1:6379/1' }
+  end
 
-Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://127.0.0.1:6379/1' }
+  Sidekiq.configure_server do |config|
+    config.redis = { :url => 'redis://127.0.0.1:6379/1' }
+  end
+else
+  Sidekiq.configure_client do |config|
+    config.redis = { :url => ENV['REDIS_URL'] }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { :url => ENV['REDIS_URL'] }
+  end
 end
