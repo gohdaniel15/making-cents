fe68e914deb1e3fefe4e14389d589138db347311
gohdaniel15 <gohdaniel15@gmail.com>
2016-08-11 14:48:35 +0800

    changed css stuff

--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20160811023851) do
     t.string   "description"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.index ["user_id"], name: "index_consultants_on_user_id", using: :btree
   end
 
   create_table "messages", force: :cascade do |t|
@@ -100,6 +101,7 @@ ActiveRecord::Schema.define(version: 20160811023851) do
   add_foreign_key "chat_rooms", "users"
   add_foreign_key "consultant_sessions", "consultants"
   add_foreign_key "consultant_sessions", "users"
+  add_foreign_key "consultants", "users"
   add_foreign_key "messages", "chat_rooms"
   add_foreign_key "messages", "users"
 end
